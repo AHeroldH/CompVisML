aeb07315be4e01001f1cd4ed19610a51938b3dcb
AHeroldH <aheroldh@gmail.com>
2019-04-15 22:36:59 +0200

    Trying cuda:0

--- cnn.py
+++ cnn.py
@@ -4,7 +4,7 @@ import torchvision
 import torchvision.transforms as transforms
 
 # Device configuration
-device = torch.device('cuda:2')
+device = torch.device('cuda:0')
 
 # Hyper parameters
 num_epochs = 5
