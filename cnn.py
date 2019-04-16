5bb4c7362828b0ebe9a8b5bcd3e9af51416f0c23
AHeroldH <aheroldh@gmail.com>
2019-04-16 13:36:17 +0200

    Revert "small correction in print"
    This reverts commit 693664b2

--- cnn.py
+++ cnn.py
@@ -135,11 +135,9 @@ for epoch in range(num_epochs):
 
     epoch_len = len(str(num_epochs))
 
-    print_msg = (f'[{epoch:>{epoch_len}}/{num_epochs:>{epoch_len}}] ' +
+    print(f'[{epoch:>{epoch_len}}/{num_epochs:>{epoch_len}}] ' +
                  f'train_loss: {train_loss:.5f} ' +
                  f'valid_loss: {valid_loss:.5f}')
-
-    print(print_msg)
 
     # clear lists to track next epoch
     train_losses = []
