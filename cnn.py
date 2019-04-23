c9cd54de21774130818e045b755ff40b830e75b5
AHeroldH <aheroldh@gmail.com>
2019-04-23 15:24:11 +0200

    filter changes fitting the kernel size

--- cnn.py
+++ cnn.py
@@ -42,21 +42,21 @@ class ConvNet(nn.Module):
     def __init__(self, num_classes=29):
         super(ConvNet, self).__init__()
         self.layer1 = nn.Sequential(
-            nn.Conv2d(3, 16, kernel_size=5, stride=1, padding=2),
-            nn.BatchNorm2d(16),
+            nn.Conv2d(3, 15, kernel_size=5, stride=1, padding=2),
+            nn.BatchNorm2d(15),
             nn.ReLU(),
             nn.MaxPool2d(kernel_size=2, stride=2))
         self.layer2 = nn.Sequential(
-            nn.Conv2d(16, 32, kernel_size=5, stride=1, padding=2),
-            nn.BatchNorm2d(32),
+            nn.Conv2d(15, 75, kernel_size=5, stride=1, padding=2),
+            nn.BatchNorm2d(75),
             nn.ReLU(),
             nn.MaxPool2d(kernel_size=2, stride=2))
         self.layer3 = nn.Sequential(
-            nn.Conv2d(32, 64, kernel_size=5, stride=1, padding=2),
-            nn.BatchNorm2d(64),
+            nn.Conv2d(75, 375, kernel_size=5, stride=1, padding=2),
+            nn.BatchNorm2d(375),
             nn.ReLU(),
             nn.MaxPool2d(kernel_size=2, stride=2))
-        self.layer4 = nn.Sequential(
+        """"self.layer4 = nn.Sequential(
             nn.Conv2d(64, 128, kernel_size=5, stride=1, padding=2),
             nn.BatchNorm2d(128),
             nn.ReLU(),
@@ -65,15 +65,15 @@ class ConvNet(nn.Module):
             nn.Conv2d(128, 256, kernel_size=5, stride=1, padding=2),
             nn.BatchNorm2d(256),
             nn.ReLU(),
-            nn.MaxPool2d(kernel_size=2, stride=2))
+            nn.MaxPool2d(kernel_size=2, stride=2))"""
         self.fc = nn.Linear(8 * 8 * 256, num_classes)
 
     def forward(self, x):
         out = self.layer1(x)
         out = self.layer2(out)
         out = self.layer3(out)
-        out = self.layer4(out)
-        out = self.layer5(out)
+        #out = self.layer4(out)
+        #out = self.layer5(out)
         out = out.reshape(out.size(0), -1)
         out = self.fc(out)
         return out
