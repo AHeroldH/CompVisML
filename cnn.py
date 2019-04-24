78a1624012f2711a72916e00cbefca9b9d32b6f8
AHeroldH <aheroldh@gmail.com>
2019-04-24 14:22:32 +0200

    added layers

--- cnn.py
+++ cnn.py
@@ -11,7 +11,7 @@ device = torch.device('cuda:0')
 # Hyper parameters
 num_epochs = 100
 num_classes = 29
-batch_size = 28
+batch_size = 30
 learning_rate = 0.0005
 
 train_dataset = torchvision.datasets.ImageFolder(root='Train/TrainImages/',
@@ -42,38 +42,62 @@ class ConvNet(nn.Module):
     def __init__(self, num_classes=29):
         super(ConvNet, self).__init__()
         self.layer1 = nn.Sequential(
-            nn.Conv2d(3, 15, kernel_size=5, stride=1, padding=2),
-            nn.BatchNorm2d(15),
+            nn.Conv2d(3, 16, kernel_size=4, stride=1, padding=2),
+            nn.BatchNorm2d(16),
             nn.ReLU(),
             nn.MaxPool2d(kernel_size=2, stride=2))
         self.layer2 = nn.Sequential(
-            nn.Conv2d(15, 75, kernel_size=5, stride=1, padding=2),
-            nn.BatchNorm2d(75),
+            nn.Conv2d(16, 32, kernel_size=4, stride=1, padding=2),
+            nn.BatchNorm2d(32),
             nn.ReLU(),
             nn.MaxPool2d(kernel_size=2, stride=2))
         self.layer3 = nn.Sequential(
-            nn.Conv2d(75, 375, kernel_size=5, stride=1, padding=2),
-            nn.BatchNorm2d(375),
+            nn.Conv2d(32, 64, kernel_size=4, stride=1, padding=2),
+            nn.BatchNorm2d(64),
             nn.ReLU(),
             nn.MaxPool2d(kernel_size=2, stride=2))
-        """"self.layer4 = nn.Sequential(
-            nn.Conv2d(64, 128, kernel_size=5, stride=1, padding=2),
+        self.layer4 = nn.Sequential(
+            nn.Conv2d(64, 128, kernel_size=4, stride=1, padding=2),
             nn.BatchNorm2d(128),
             nn.ReLU(),
             nn.MaxPool2d(kernel_size=2, stride=2))
         self.layer5 = nn.Sequential(
-            nn.Conv2d(128, 256, kernel_size=5, stride=1, padding=2),
+            nn.Conv2d(128, 192, kernel_size=4, stride=1, padding=2),
+            nn.BatchNorm2d(192),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2))
+        self.layer6 = nn.Sequential(
+            nn.Conv2d(192, 256, kernel_size=4, stride=1, padding=2),
             nn.BatchNorm2d(256),
             nn.ReLU(),
-            nn.MaxPool2d(kernel_size=2, stride=2))"""
-        self.fc = nn.Linear(8 * 8 * 256, num_classes)
+            nn.MaxPool2d(kernel_size=2, stride=2))
+        #self.layer7 = nn.Sequential(
+        #    nn.Conv2d(160, 192, kernel_size=8, stride=1, padding=4),
+        #    nn.BatchNorm2d(192),
+        #    nn.ReLU(),
+        #    nn.MaxPool2d(kernel_size=2, stride=2))
+        #self.layer8 = nn.Sequential(
+        #   nn.Conv2d(192, 224, kernel_size=8, stride=1, padding=4),
+        #   nn.BatchNorm2d(224),
+        #   nn.ReLU(),
+        #   nn.MaxPool2d(kernel_size=2, stride=2))
+        #self.layer9 = nn.Sequential(
+        #   nn.Conv2d(224, 256, kernel_size=8, stride=1, padding=4),
+        #   nn.BatchNorm2d(256),
+        #   nn.ReLU(),
+        #   nn.MaxPool2d(kernel_size=2, stride=2))
+        self.fc = nn.Linear(4 * 4 * 256, num_classes)
 
     def forward(self, x):
         out = self.layer1(x)
         out = self.layer2(out)
         out = self.layer3(out)
-        #out = self.layer4(out)
-        #out = self.layer5(out)
+        out = self.layer4(out)
+        out = self.layer5(out)
+        out = self.layer6(out)
+        #out = self.layer7(out)
+        #out = self.layer8(out)
+        #out = self.layer9(out)
         out = out.reshape(out.size(0), -1)
         out = self.fc(out)
         return out
