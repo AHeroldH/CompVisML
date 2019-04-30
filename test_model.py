c4c6aa6a93db92820559f3e20b522e794ac2ffb6
AHeroldH <aheroldh@gmail.com>
2019-04-30 14:04:01 +0200

    test_model implemented

---
+++ test_model.py
@@ -0,0 +1,63 @@
+import torch
+import torch.nn as nn
+from PIL import Image
+import torchvision.transforms as transforms
+
+
+class ConvNet(nn.Module):
+    def __init__(self, num_classes=29):
+        super(ConvNet, self).__init__()
+        self.layer1 = nn.Sequential(
+            nn.Conv2d(3, 16, kernel_size=4, stride=1, padding=2),
+            nn.BatchNorm2d(16),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2))
+        self.layer2 = nn.Sequential(
+            nn.Conv2d(16, 32, kernel_size=4, stride=1, padding=2),
+            nn.BatchNorm2d(32),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2))
+        self.layer3 = nn.Sequential(
+            nn.Conv2d(32, 64, kernel_size=4, stride=1, padding=2),
+            nn.BatchNorm2d(64),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2))
+        self.layer4 = nn.Sequential(
+            nn.Conv2d(64, 128, kernel_size=4, stride=1, padding=2),
+            nn.BatchNorm2d(128),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2))
+        self.layer5 = nn.Sequential(
+            nn.Conv2d(128, 192, kernel_size=4, stride=1, padding=2),
+            nn.BatchNorm2d(192),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2))
+        self.layer6 = nn.Sequential(
+            nn.Conv2d(192, 256, kernel_size=4, stride=1, padding=2),
+            nn.BatchNorm2d(256),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2))
+        self.fc = nn.Linear(4 * 4 * 256, num_classes)
+
+    def forward(self, x):
+        out = self.layer1(x)
+        out = self.layer2(out)
+        out = self.layer3(out)
+        out = self.layer4(out)
+        out = self.layer5(out)
+        out = self.layer6(out)
+        out = out.reshape(out.size(0), -1)
+        out = self.fc(out)
+        return out
+
+
+device = torch.device("cuda:0")
+model = ConvNet(29)
+model.load_state_dict(torch.load(model.ckpt))
+model.to(device)
+
+im = Image.open('Validation/ValidationImages/9/771')
+im = transforms.functional.to_tensor(im).to(device)
+im = torch.stack([im])
+
+model(im)
