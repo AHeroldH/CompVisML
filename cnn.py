fd438abd75650277ce5df40286fd5726dd4d8b4a
AHeroldH <aheroldh@gmail.com>
2019-05-02 08:32:21 +0200

    Densenet161 - added transformations

--- cnn.py
+++ cnn.py
@@ -26,11 +26,12 @@ learning_rate = 0.001
 train_transforms = transforms.Compose([
         transforms.RandomResizedCrop(224),
         transforms.RandomHorizontalFlip(),
+        transforms.RandomPerspective(),
+        transforms.ColorJitter(),
         transforms.ToTensor()
     ])
 
 valid_transform = transforms.Compose([
-        transforms.Resize(256),
         transforms.CenterCrop(224),
         transforms.ToTensor()
     ])
@@ -41,12 +42,7 @@ train_dataset = torchvision.datasets.ImageFolder(root='Train/TrainImages',
 valid_dataset = torchvision.datasets.ImageFolder(root='Validation/ValidationImages',
                                                  transform=valid_transform)
 
-
 class_names = train_dataset.classes
-
-# test_dataset = 'Test/TestImages'
-# test_data_files = os.listdir(test_dataset)
-# im = Image.open(f'{test_dataset}/{test_data_files[0]}')
 
 dataset = 'Test/TestImages'
 
@@ -72,33 +68,6 @@ def loader(path):
 
 
 class DatasetFolder:
-    """A generic data loader where the samples are arranged in this way: ::
-        root/class_x/xxx.ext
-        root/class_x/xxy.ext
-        root/class_x/xxz.ext
-        root/class_y/123.ext
-        root/class_y/nsdf3.ext
-        root/class_y/asd932_.ext
-    Args:
-        root (string): Root directory path.
-        loader (callable): A function to load a sample given its path.
-        extensions (tuple[string]): A list of allowed extensions.
-            both extensions and is_valid_file should not be passed.
-        transform (callable, optional): A function/transform that takes in
-            a sample and returns a transformed version.
-            E.g, ``transforms.RandomCrop`` for images.
-        target_transform (callable, optional): A function/transform that takes
-            in the target and transforms it.
-        is_valid_file (callable, optional): A function that takes path of an Image file
-            and check if the file is a valid_file (used to check of corrupt files)
-            both extensions and is_valid_file should not be passed.
-     Attributes:
-        classes (list): List of the class names.
-        class_to_idx (dict): Dict with items (class_name, class_index).
-        samples (list): List of (sample path, class_index) tuples
-        targets (list): The class_index value for each image in the dataset
-    """
-
     def __init__(self):
         super(DatasetFolder, self).__init__()
         ids, samples = make_dataset(dataset)
@@ -107,12 +76,6 @@ class DatasetFolder:
         self.ids = ids
 
     def __getitem__(self, index):
-        """
-        Args:
-            index (int): Index
-        Returns:
-            tuple: (sample, target) where target is class_index of the target class.
-        """
         ids = self.ids
         path = self.samples[index]
         sample = loader(path)
@@ -137,77 +100,24 @@ test_loader = torch.utils.data.DataLoader(dataset=DatasetFolder(),
                                           batch_size=1,
                                           shuffle=False)
 
-class ConvNet(nn.Module):
-    def __init__(self, num_classes=29):
-        super(ConvNet, self).__init__()
-        self.layer1 = nn.Sequential(
-            nn.Conv2d(3, 16, kernel_size=4, stride=1, padding=2),
-            nn.BatchNorm2d(16),
-            nn.ReLU(),
-            nn.MaxPool2d(kernel_size=2, stride=2))
-        self.layer2 = nn.Sequential(
-            nn.Conv2d(16, 32, kernel_size=4, stride=1, padding=2),
-            nn.BatchNorm2d(32),
-            nn.ReLU(),
-            nn.MaxPool2d(kernel_size=2, stride=2))
-        self.layer3 = nn.Sequential(
-            nn.Conv2d(32, 64, kernel_size=4, stride=1, padding=2),
-            nn.BatchNorm2d(64),
-            nn.ReLU(),
-            nn.MaxPool2d(kernel_size=2, stride=2))
-        self.layer4 = nn.Sequential(
-            nn.Conv2d(64, 128, kernel_size=4, stride=1, padding=2),
-            nn.BatchNorm2d(128),
-            nn.ReLU(),
-            nn.MaxPool2d(kernel_size=2, stride=2))
-        self.layer5 = nn.Sequential(
-            nn.Conv2d(128, 192, kernel_size=4, stride=1, padding=2),
-            nn.BatchNorm2d(192),
-            nn.ReLU(),
-            nn.MaxPool2d(kernel_size=2, stride=2))
-        self.layer6 = nn.Sequential(
-            nn.Conv2d(192, 256, kernel_size=4, stride=1, padding=2),
-            nn.BatchNorm2d(256),
-            nn.ReLU(),
-            nn.MaxPool2d(kernel_size=2, stride=2))
-        self.fc = nn.Linear(4 * 4 * 256, num_classes)
-
-    def forward(self, x):
-        out = self.layer1(x)
-        out = self.layer2(out)
-        out = self.layer3(out)
-        out = self.layer4(out)
-        out = self.layer5(out)
-        out = self.layer6(out)
-        out = out.reshape(out.size(0), -1)
-        out = self.fc(out)
-        return out
-
-
-model_conv = ConvNet(num_classes).to(device)
-
-# Loss and optimizer
-criterion = nn.CrossEntropyLoss()
-optimizer_conv = torch.optim.Adamax(model_conv.parameters(), lr=learning_rate)
-
-#model_conv = torchvision.models.densenet161(pretrained=True)
-#for param in model_conv.parameters():
-#    param.requires_grad = False
+model_conv = torchvision.models.densenet161(pretrained=True)
+for param in model_conv.parameters():
+    param.requires_grad = False
 
 # Parameters of newly constructed modules have requires_grad=True by default
-#num_ftrs = model_conv.classifier.in_features
-#model_conv.classifier = nn.Linear(num_ftrs, num_classes)
+num_ftrs = model_conv.classifier.in_features
+model_conv.classifier = nn.Linear(num_ftrs, num_classes)
 
-#model_conv = model_conv.to(device)
+model_conv = model_conv.to(device)
 
-#criterion = nn.CrossEntropyLoss()
+criterion = nn.CrossEntropyLoss()
 
 # Observe that only parameters of final layer are being optimized as
 # opposed to before.
-#optimizer_conv = optim.SGD(model_conv.classifier.parameters(), lr=learning_rate, momentum=0.9)
+optimizer_conv = optim.SGD(model_conv.classifier.parameters(), lr=learning_rate, momentum=0.9)
 
 # Decay LR by a factor of 0.1 every 7 epochs
-#exp_lr_scheduler = lr_scheduler.StepLR(optimizer_conv, step_size=7, gamma=0.1)
+exp_lr_scheduler = lr_scheduler.StepLR(optimizer_conv, step_size=7, gamma=0.1)
 
 # Train the model
 
@@ -291,48 +201,8 @@ time_elapsed = time.time() - since
 print('Training complete in {:.0f}m {:.0f}s'.format(time_elapsed // 60, time_elapsed % 60))
 print('Best val Acc: {:4f}'.format(best_acc))
 
-# clear lists to track next epoch
-# train_losses = []
-# valid_losses = []
-
-# early_stopping needs the validation loss to check if it has decreased,
-# and if it has, it will make a checkpoint of the current model
-# early_stopping(valid_loss, model_conv)
-
-# if early_stopping.early_stop:
-#    print("Early stopping")
-#    break
-
 
 # Test the model
-
-'''def apply_test_transforms(inp):
-    # out = transforms.functional.resize(inp, [224, 224])
-    out = transforms.functional.to_tensor(inp)
-    # mean = torch.tensor([0.485, 0.456, 0.406], dtype=torch.float32, device=device)
-    # std = torch.tensor([0.229, 0.224, 0.225], dtype=torch.float32, device=device)
-    # out = transforms.functional.normalize(out, mean, std)
-    return out.to(device)
-
-
-def predict_single_instance(model, tensor):
-    batch = torch.stack([tensor])
-    preds = model(batch)
-    _, predictions = torch.max(preds, 1)
-    return predictions.item() + 1
-
-
-def test_data_from_fname(fname):
-    im = Image.open('{}/{}'.format(test_dataset, fname))
-    print(im)
-    return apply_test_transforms(im)
-
-
-def extract_file_id(fname):
-    return int(re.search('\d+', fname).group())
-    
-predicts = {extract_file_id(fname): predict_single_instance(model_conv, test_data_from_fname(fname))
-            for fname in test_data_files}'''
 
 model_conv.eval()  # eval mode (batchnorm uses moving mean/variance instead of mini-batch mean/variance)
 
@@ -371,13 +241,6 @@ df = read.sort_index()
 df['ID'] = df.index
 df = df[['ID','Label']]
 df.to_csv('submission.csv', index=False)
-
-'''ds = pd.Series({id: label for (id, label) in zip(predicts.keys(), predicts.values())})
-df = pd.DataFrame(ds, columns=['Label']).sort_index()
-df['ID'] = df.index
-df = df[['ID', 'Label']]
-
-df.to_csv('submission.csv', index=False)'''
 
 # Save the model checkpoint
 torch.save(model_conv.state_dict(), 'model.ckpt')
